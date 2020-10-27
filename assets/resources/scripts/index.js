3ad9bea839fca0e3c78e09bd06756ecde08ed552
joaorafas <61762276+joaorafas@users.noreply.github.com>
2020-10-27 04:53:42 -0300

    Update October

---
+++ assets/resources/scripts/index.js
@@ -0,0 +1,26 @@
+
+
+window.onload = () => alert("Página carregada");
+
+
+// prompt 
+function home() {
+    name = prompt("Olá, digite seu nome");
+}
+
+/**
+ * Função anonima sem argumento
+ * Alert 
+ */
+let about = function() {
+    alert('Olá ' + name + ', seja bem vindo ao UTFtorneios! ' +
+'Este é um projeto para criação de torneios de games ' +
+'para a comunidade local de Guarapuava e região.');
+}
+
+
+// function anonima com argumento
+consoleWarning = function(event) {
+    console.log("Página em construção");
+}
+
